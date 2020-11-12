e37f94fe1b6485cf199c8fabe4752d55e4cad604
antonio rodriguez <antoniorg1985@hotmail.com>
2020-11-12 18:51:54 +0100

    First commit

---
+++ src/es/studium/Aleatorios/Aleatorios.java
@@ -0,0 +1,13 @@
+package es.studium.Aleatorios;
+
+public class Aleatorios
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Nummero aleatorio: " + Math.random());
+		System.out.println("Otro numero aleatorio: " + Math.random());
+
+	}
+
+}
